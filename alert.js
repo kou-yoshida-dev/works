e3e059bad3c602b6e141ae6e090933e6ab86dc82
吉田航 <yoshidakou@yoshidawatarunoMacBook-Air.local>
2020-12-30 13:36:49 +0900

    [update]Webアプリを作ってみよう-1

--- alert.js
+++ alert.js
@@ -1,1 +1,18 @@
 
+
+var hoge;
+
+var name = taro;
+
+
+
+function loadHello(){
+    alert("Hello!");
+}
+  
+  function sum(){
+    var x = 3;
+    var y = 4;
+    var z = x + y;
+    alert(z);
+  }
